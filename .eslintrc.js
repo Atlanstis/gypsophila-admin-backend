6b543526f7069eb3c20d79226982e054455a7e8f
Gypsophila。 <futur_wxc@163.com>
2022-08-30 11:05:23 +0800

    chore: 初始化

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['standard', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+};

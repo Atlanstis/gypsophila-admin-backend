1b87baf421bab21074a1f8cfd4413680c829f3ea
Gypsophila <futur_wxc@163.com>
2023-01-16 15:36:54 +0800

    feat(数据库): 通过 TypeOrm 对数据库进行连接

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { getConfig } from './utils';
 
 @Module({
-  imports: [ConfigModule.forRoot({ ignoreEnvFile: true, isGlobal: true, load: [getConfig] })],
+  imports: [
+    ConfigModule.forRoot({ ignoreEnvFile: true, isGlobal: true, load: [getConfig] }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const config = configService.get('mysql');
+        return {
+          ...config,
+          entities: [],
+          logging: ['error'],
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
